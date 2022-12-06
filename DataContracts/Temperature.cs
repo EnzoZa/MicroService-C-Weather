b18558656803b437aadbb48e9e664ff6584a923d
Enzo Zampaglione <71082476+EnzoZa@users.noreply.github.com>
2022-12-06 10:03:51 +0100

    DataContract

---
+++ DataContracts/Temperature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts
+{
+    public class Temperature
+    {
+        public String City { get; set; }
+        public Int32 Temp { get; set; }
+    }
+}
