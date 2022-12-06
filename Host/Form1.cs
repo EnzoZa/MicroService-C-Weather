159ec7faebf75eddcc72f2a329279127ec08595d
Enzo Zampaglione <71082476+EnzoZa@users.noreply.github.com>
2022-12-06 10:04:32 +0100

    Host

---
+++ Host/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Host
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ServiceHost host = new ServiceHost(typeof(MeteoService));
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            host.Open();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            host?.Close();
+        }
+    }
+}
